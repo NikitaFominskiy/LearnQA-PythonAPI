b85b4c5a95db3fd02e497d07c0e9d8639653de38
NikitaFominskiy <zolko.zolotusik@gmail.com>
2025-09-17 13:25:37 +0400

    2st step

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-import requests
+json_text = {
+    "messages":[
+        {"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},
+        {"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]
+}
 
-payload = {"login":"secret_login", "password":"secret_pass2"}
-response1 = requests.post("https://playground.learnqa.ru/api/get_auth_cookie", data=payload)
-
-cookie_value = response1.cookies.get('auth_cookie')
-
-cookies = {}
-if cookie_value is not None:
-    cookies.update({'auth_cookie': cookie_value})
-
-response2 = requests.post("https://playground.learnqa.ru/api/check_auth_cookie", cookies=cookies)
-
-print(response2.text)
+print(json_text['messages'][1])
