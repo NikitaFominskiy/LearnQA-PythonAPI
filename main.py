27c1dad12559b83e5c847a551a3cc56f7c03cb77
NikitaFominskiy <zolko.zolotusik@gmail.com>
2025-09-17 14:19:09 +0400

    6dz

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-json_text = {
-    "messages":[
-        {"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},
-        {"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]
-}
+import requests
 
-print(json_text['messages'][1])
+response = requests.get("https://playground.learnqa.ru/api/long_redirect",)
+print(f"Count: {len(response.history)}")
+print(f"URL: {response.url}")
